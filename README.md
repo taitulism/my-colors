eb34ee4f7206647e74bee5a23b50d227215b6803
Itai Tenenbaum <taitulism@gmail.com>
2020-03-15 22:16:55 +0200

    docs

--- README.md
+++ README.md
@@ -9,24 +9,25 @@ Returns a colorizer function.
 
 Both foreground & background arguments expect a color.
 A color could be one of the following:
-* A supported color name (see list below)
-* 0-255 Number - [256 color palette](https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg)
+* A supported color name ([see list below](#supported-safe-color-names))
+* 0-255 Number - [see 256 color palette](https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg)
 * HEX - a hex color string that starts with a hash sign (e.g. `'#1d3ddf'`)
 * RGB - an array of three values (e.g. `[200, 40, 40]`)
 
 Example:
 ```js
 // my-app.js
-const createColorizer = require('node-colors');
-const warning = createColorizer('yellow', 'red', 'bold');
+const createColor = require('node-colors');
+
+const warning = createColor('yellow', 'red', 'bold');
 const msg = warning('Attention: Do not do that.');
 
 console.log(msg)
 ```
 ![Terminal Example](/example.png)
 
-Supported Color Names
----------------------
+Supported Safe Color Names
+--------------------------
 * Black
 * Red
 * Green
@@ -36,18 +37,18 @@ Supported Color Names
 * Cyan
 * White
 
-### 16 colors terminals
-You can add `'bright'` before each color to use its bright mode e.g. `'brightBlue'`. Casing is ignored.
-
 Modifiers
 ---------
-> Note: Support may vary between different OS/terminals
-* Bold
-* Underline
-* Reverse
-<!-- * Dim -->
+> **Note:** Support may vary between different OS/terminals
 
-<!-- 
-### TODO:
-You can also use the first letter e.g. `B` for Bold. 
--->
+* bold
+* dim
+* italic
+* underline
+* invert
+* strike
+
+Example:
+```js
+createColor('#000000', [77, 160, 255], ['bold', 'underline']);
+```
--- playground/play.js
+++ playground/play.js
@@ -1,50 +1,12 @@
-/* eslint-disable max-statements */
-/* eslint-disable max-lines-per-function */
-/* eslint-disable no-console */
-const safePalette = require('../src/safe-palette');
-const palette256 = require('../src/256-palette');
-
+/* eslint-disable max-statements, max-lines-per-function, no-console */
+const createColor = require('../');
 const TEXT = 'This is the message';
 
-// Safe Colors
-/*
-const black   = safePalette.colors.black('BLACK');
-const red     = safePalette.colors.red('RED');
-const green   = safePalette.colors.green('GREEN');
-const yellow  = safePalette.colors.yellow('YELLOW');
-const blue    = safePalette.colors.blue('BLUE');
-const magenta = safePalette.colors.magenta('MAGENTA');
-const cyan    = safePalette.colors.cyan('CYAN');
-const white   = safePalette.colors.white('WHITE');
+const x = createColor('#000000', [77, 160, 255], ['bold', 'underline']);
+const y = createColor('#000000', [77, 160, 255], ['underline']);
 
-const boldBlack   = safePalette.colors.bold.black('BOLD BLACK');
-const boldRed     = safePalette.colors.bold.red('BOLD RED');
-const boldGreen   = safePalette.colors.bold.green('BOLD GREEN');
-const boldYellow  = safePalette.colors.bold.yellow('BOLD YELLOW');
-const boldBlue    = safePalette.colors.bold.blue('BOLD BLUE');
-const boldMagenta = safePalette.colors.bold.magenta('BOLD MAGENTA');
-const boldCyan    = safePalette.colors.bold.cyan('BOLD CYAN');
-const boldWhite   = safePalette.colors.bold.white('BOLD WHITE');
-
-console.log(black);
-console.log(red);
-console.log(green);
-console.log(yellow);
-console.log(blue);
-console.log(magenta);
-console.log(cyan);
-console.log(white);
-
-console.log(boldBlack);
-console.log(boldRed);
-console.log(boldGreen);
-console.log(boldYellow);
-console.log(boldBlue);
-console.log(boldMagenta);
-console.log(boldCyan);
-console.log(boldWhite);
-*/
-
+console.log(x(TEXT));
+console.log(y(TEXT));
 
 // 256 colors
 function print256 () {
@@ -57,7 +19,7 @@ function print256 () {
 
 		for (let j = 0; j < 8; j++) {
 			const colorNumber = i + j;
-			const colorize = palette256.createColor(0, colorNumber);
+			const colorize = createColor(0, colorNumber);
 			const colorTxt = colorNumber < 10 ? '  ' + colorNumber : ' ' + colorNumber;
 			const msg = colorize(` ${colorTxt} `);
 
@@ -72,7 +34,7 @@ function print256 () {
 
 		for (let j = 0; j < perRow; j++) {
 			const colorNumber = i + j;
-			const colorize = palette256.createColor(0, colorNumber);
+			const colorize = createColor(0, colorNumber);
 			const colorTxt = colorNumber < 100 ? ' ' + colorNumber : colorNumber;
 			const msg = colorize(` ${colorTxt} `);
 
@@ -89,7 +51,7 @@ function print256 () {
 		for (let j = 0; j < perRow; j++) {
 			const colorNumber = i + j;
 			const fg = i < 244 ? 7 : 0;
-			const colorize = palette256.createColor(fg, colorNumber);
+			const colorize = createColor(fg, colorNumber);
 			const msg = colorize(` ${colorNumber} `);
 
 			colorAry.push(msg);
@@ -99,28 +61,3 @@ function print256 () {
 }
 
 print256();
-
-/*
-transparent 0
-black 16
-red 1, 160, 196
-green 2
-light green 2, 28, 34
-yellow 3, 154, 190, 184, 220, 226
-blue 4, 20, 21
-light blue 12, 27, 33
-pink 5, 200, 201
-turquise 6, 44
-orange 202, 208, 214
-purple 90, 91,
-brown 130
-*/
-
-
-const msg1 = palette256.colors.turquise1(TEXT);
-const msg2 = palette256.colors.turquise2(TEXT);
-// const msg3 = palette256.colors.pink(TEXT);
-
-console.log(msg1);
-console.log(msg2);
-// console.log(msg3);
